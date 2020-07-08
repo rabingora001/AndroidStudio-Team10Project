2f191b4bbc76ed6a9db2fca0e4aa484409e2669b
rabin001 <rabin.gora@gmail.com>
2020-07-08 03:09:29 -0700

    commit by first member after merging pull request from second member. click login btn

--- app/src/main/java/edu/tacoma/uw/team10project/danvityActivity.java
+++ app/src/main/java/edu/tacoma/uw/team10project/danvityActivity.java
@@ -3,6 +3,7 @@ package edu.tacoma.uw.team10project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class danvityActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class danvityActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_danvity);
+
+        //added second commit over here
+        TextView newTextlines = findViewById(R.id.second_textview);
+        newTextlines.setText("2) This is the commit by rabin001 after\n merging " +
+                "danvity's pull request");
     }
 }
