39445cfdf955ff8a747b31147bba96e4b2ee4a0e
danmanfan <danvity@uw.edu>
2020-07-08 00:05:06 -0700

    Changed main activity, created an activity with my uw user name. The login button calls for the activity that I created.

--- app/src/main/java/edu/tacoma/uw/team10project/MainActivity.java
+++ app/src/main/java/edu/tacoma/uw/team10project/MainActivity.java
@@ -1,13 +1,14 @@
 package edu.tacoma.uw.team10project;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import java.awt.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,18 +20,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i(TAG, "onCreate");
+//        Button loginBtn = findViewById(R.id.login_btn);
+//        loginBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(view.getContext(), MyDashboardPage.class);
+//                EditText userNameTextId = (EditText) findViewById(R.id.userName_txt);
+//                String userNameText = userNameTextId.getText().toString();
+//                intent.putExtra(EXTRA_STRING, userNameText);
+//                startActivity(intent);
+//            }
+//        });
+    }
 
-        Button loginBtn = findViewById(R.id.login_btn);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(view.getContext(), MyDashboardPage.class);
-                EditText userNameTextId = (EditText) findViewById(R.id.userName_txt);
-                String userNameText = userNameTextId.getText().toString();
-                intent.putExtra(EXTRA_STRING, userNameText);
-                startActivity(intent);
-            }
-        });
-
+    /** Called when the user taps the Text button */
+    public void loginButton(View view) {
+        Intent intent = new Intent(this, danvityActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/edu/tacoma/uw/team10project/danvityActivity.java
@@ -0,0 +1,14 @@
+package edu.tacoma.uw.team10project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class danvityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_danvity);
+    }
+}
